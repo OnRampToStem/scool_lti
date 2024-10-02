cd9b53cb8fe0ca2a535f337061fa45d2480f0f29
John Wagenleitner <johnwa@mail.fresnostate.edu>
2024-10-02 10:59:41 -0700

    bump docker Python to 3.12.7

--- scool/__init__.py
+++ scool/__init__.py
@@ -14,4 +14,4 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-__version__ = "24.9.30"
+__version__ = "24.10.2"
